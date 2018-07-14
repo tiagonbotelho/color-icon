c86cf47a5e295fbb0410a5902ab1c42255df0321
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-07-14 16:50:12 +0100

    Change color palette

--- src/bg/helpers.js
+++ src/bg/helpers.js
@@ -1,11 +1,11 @@
 function colorToHex(color) {
     switch(color) {
-        case "red": return "#FB635E";
-        case "green": return "#86E26D";
-        case "blue": return "#53BDF6";
-        case "yellow": return "#FDD74F";
-        case "orange": return "#FCAB47";
-        case "purple": return "#D58FE3";
+        case "pink": return "#D37E8B";
+        case "green": return "#B5DC73";
+        case "blue": return "#81C6C6";
+        case "yellow": return "#FFCD6F";
+        case "orange": return "#F5903F";
+        case "purple": return "#895374";
     }
 }
 
